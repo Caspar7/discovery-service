933dc7362d2addac1a772acc31af97cd66893fe3
Yunchao Du <ycdu@thoughtworks.com>
2019-02-11 14:16:10 +0800

    update deploy sh

--- deploy.sh
+++ deploy.sh
@@ -20,4 +20,4 @@ fi
 
 docker build -t ${serviceName}:$BUILD_NUMBER .
 
-docker run --env env=${env} -it -d -p 1111:1111 --name ${serviceName} ${serviceName}:v$BUILD_NUMBER
+docker run --env env=${env} -it -d -p 1111:1111 --name ${serviceName} ${serviceName}:$BUILD_NUMBER
