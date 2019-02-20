31cfc0ad2384f5ced6b7f488eb88d6f3b1ed41d9
Yunchao Du <ycdu@thoughtworks.com>
2019-02-20 17:30:26 +0800

    add evn para

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -22,4 +22,4 @@ echo "load docker images ${serviceName}_${BUILD_NUMBER}.tar .."
 docker load -i ${serviceName}_${BUILD_NUMBER}.tar
 
 echo "run docker container..."
-docker run --env env=${env} -it -d -p 1111:1111 --name ${serviceName} ${serviceName}:${BUILD_NUMBER}
+docker run --env env=${env} --env eurekaServerZone=${eurekaServerZone} -it -d -p 1111:1111 --name ${serviceName} ${serviceName}:${BUILD_NUMBER}
